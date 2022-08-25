07270af53607b23e5e57b98eeda5ce3f74033be1
Laurien Garcia <00106082@aluno.uniso.br>
2022-08-24 22:23:24 -0300

    Adicionar arquivos de projeto.

---
+++ Aula-Backend/Disciplina.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1_17_08
+{
+    public enum SituacaoAluno
+    {
+        Pendente, Aprovado, Reprovado, Exame
+    }
+    public class Disciplina
+    {
+        public string Nome { get; set; }
+        public int Codigo { get; set; }
+        public int Cargahoraria { get; set; }
+        public SituacaoAluno Situacao { get; set; }
+
+        public List<Aluno> ListAlunos { get; set; }
+
+        public Dictionary<Aluno, double> MapaDeNotas { get; set; }
+        public Dictionary<Aluno, int> MapaDeFaltas { get; set; }
+        public Dictionary<Aluno, SituacaoAluno> MapaSituacao { get; set; }
+        public int Faltas { get; set; }
+
+        public Disciplina(String Nome, int Codigo, int Cargahoraria)
+        {
+            this.Nome = Nome;
+            this.Codigo = Codigo;
+            this.Cargahoraria = Cargahoraria;
+
+            ListAlunos = new List<Aluno>();
+            MapaDeNotas = new Dictionary<Aluno, double>();
+            MapaDeFaltas = new Dictionary<Aluno, int>();
+            MapaSituacao = new Dictionary<Aluno, SituacaoAluno>();
+        }
+
+        public void LancarNota(Aluno aluno, double np1, double np2, double trabalho)
+        {
+            double Notafinal = 0;
+            Notafinal = (np1 * 0.3) + (np2 * 0.3) + (trabalho * 0.4);
+            MapaDeNotas[aluno] = Notafinal;
+        }
+        public Aluno LocalizarAluno(String strBusca)
+        {
+            strBusca = strBusca.ToUpper();
+            bool isCod = false;
+            int cod = -1;
+            try
+            {
+                cod = Convert.ToInt32(strBusca);
+                isCod = true;
+            }
+            catch
+            {
+                isCod = false;
+            }
+
+            if (isCod == true)
+            {
+                return ListAlunos.First(o => o.Matricula == cod);
+            }
+
+            return ListAlunos.First(o => o.Nome.Equals(strBusca));
+        }
+
+        public void CadastrarAlunoViaConsole()
+        {
+            Aluno novoAluno = new Aluno();
+            novoAluno.CadastrarAlunoViaConsole();
+            if (this.CadastrarAluno(novoAluno))
+            {
+                Console.WriteLine("Cadastro efetuado com sucesso");
+            }
+            else
+                Console.WriteLine("Erro ao efetuar o cadastro");
+        }
+
+        public bool CadastrarAluno(Aluno novoAluno)
+        {
+
+            //  for(int i=0; i<ListAlunos.Count; i++)
+            //{
+            //    if (ListAlunos[i].Nome.Equals(novoAluno.Nome))//aluno já cadastrado
+            //        return false; //indica erro ao cadastrar nome já existe
+            // }
+
+            novoAluno.Nome = novoAluno.Nome.ToUpper();
+            Aluno alunoEncontrado = ListAlunos.FirstOrDefault(aluno => aluno.Nome.Equals(novoAluno.Nome));
+            if (alunoEncontrado != null)
+                return false;
+
+            ListAlunos.Add(novoAluno);
+            MapaDeNotas.Add(novoAluno, 0);
+            MapaDeFaltas.Add(novoAluno, 0);
+            MapaSituacao.Add(novoAluno,0);
+
+            return true;
+        }
+
+        public void AddFalta(Aluno aluno)
+        {
+            MapaDeFaltas[aluno]++;
+        }
+
+        public void ConsultarAprovadoReprovado()
+        {
+            foreach (KeyValuePair<Aluno, int> alunoatual in MapaDeFaltas)
+            {
+                int aux = Convert.ToInt32(Cargahoraria / 5);
+                if (alunoatual.Value <= aux)//VERIFICANDO AS FALTAS
+                {
+                    // APROVADO POR FALTAS
+                    if (MapaDeNotas[alunoatual.Key] >= 6) // VERIFICANDO A NOTA
+                    {
+                        //APROVADO por nota
+                        alunoatual.Key.Situacao = "Aprovado";
+                        Console.WriteLine($"Aluno: {alunoatual.Key.Nome}");
+                        Console.WriteLine($"Matricula: {alunoatual.Key.Matricula}");
+                        Console.WriteLine($"Curso: {alunoatual.Key.Curso}");
+                        Console.WriteLine($"Faltas: {alunoatual.Value}");
+                        Console.WriteLine($"Situacao: {alunoatual.Key.Situacao}");
+                    }
+                    else
+                    {
+                        alunoatual.Key.Situacao = "Reprovado";
+                        Console.WriteLine($"Aluno: {alunoatual.Key.Nome}");
+                        Console.WriteLine($"Matricula: {alunoatual.Key.Matricula}");
+                        Console.WriteLine($"Curso: {alunoatual.Key.Curso}");
+                        Console.WriteLine($"Faltas: {alunoatual.Value}");
+                        Console.WriteLine($"Situacao: {alunoatual.Key.Situacao}");
+                    }
+                }
+                else
+                {
+                    //Reprovado por Falta
+                    alunoatual.Key.Situacao = "Reprovado";
+                    Console.WriteLine($"Aluno: {alunoatual.Key.Nome}");
+                    Console.WriteLine($"Matricula: {alunoatual.Key.Matricula}");
+                    Console.WriteLine($"Curso: {alunoatual.Key.Curso}");
+                    Console.WriteLine($"Faltas: {alunoatual.Value}");
+                    Console.WriteLine($"Situacao: {alunoatual.Key.Situacao}");
+
+                }
+            }
+        }
+
+    }
+}
---
+++ Aula-Backend/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula1_17_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Disciplina disciplina = new Disciplina("Backend", 777, 20);
+            string entrada;
+            Aluno aluno;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Entre com a opcao desejada:");
+                Console.WriteLine("1 Cadastrar aluno");
+                Console.WriteLine("2 Lancar Nota");
+                Console.WriteLine("3 Consultar Nota");
+                Console.WriteLine("4 Listar Notas");
+                Console.WriteLine("5 Inserir Falta");
+                Console.WriteLine("6 Consultar Faltas");
+               // Console.WriteLine("7 Listar Aprovados/Reprovados");
+                Console.WriteLine("Sair (Para encerrar)");
+                entrada = Console.ReadLine();
+
+                switch (entrada)
+                {
+                    //cadastro
+                    case "1":
+                        disciplina.CadastrarAlunoViaConsole();
+
+                        break;
+
+                    //lancar nota
+                    case "2":
+                        try
+                        {
+                            Console.WriteLine("Entre com Nome ou Matricula do Aluno para lancar nota:");
+                            aluno = disciplina.LocalizarAluno(Console.ReadLine());
+                            Console.WriteLine($"Localizado: Aluno: {aluno.Nome} Matricula: {aluno.Matricula}");
+
+                            Console.WriteLine("Entre com a nota np1:");
+                            double np1 = Convert.ToDouble(Console.ReadLine());
+
+                            Console.WriteLine("Entre com a nota np2:");
+                            double np2 = Convert.ToDouble(Console.ReadLine());
+
+                            Console.WriteLine("Entre com a nota Trabalho:");
+                            double trabalho = Convert.ToDouble(Console.ReadLine());
+
+                            disciplina.LancarNota(aluno, np1, np2, trabalho);
+                            Console.WriteLine("Nota Final: " + disciplina.MapaDeNotas[aluno]);
+                            Console.WriteLine("Nota cadastrada com sucesso! ");
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Aluno nao Encontrado!");
+                        }
+                    
+                        break;
+
+                    //consultar nota
+                    case "3":
+                        Console.WriteLine("Entre com Nome ou Matricula do Aluno para Consulta nota:");
+                        aluno = disciplina.LocalizarAluno(Console.ReadLine());
+                        Console.WriteLine($" Aluno: {aluno.Nome} Matricula: {aluno.Matricula} Nota Final:  {disciplina.MapaDeNotas[aluno]}");
+                        break;
+
+                    //listar nota
+                    case "4":
+                        Console.WriteLine("Listagem de Notas: ");
+                        foreach (KeyValuePair<Aluno, double> alunoAtual in disciplina.MapaDeNotas)
+                        {
+                            Console.WriteLine($"Aluno: {alunoAtual.Key.Nome} Nota Final: {alunoAtual.Value}");
+                        }
+                        break;
+
+                    //inserir falta
+                    case "5":
+                        try
+                        {
+                            Console.WriteLine("Entre com Nome ou Matricula do Aluno para Consulta nota:");
+                            aluno = disciplina.LocalizarAluno(Console.ReadLine());
+                            disciplina.AddFalta(aluno);
+                            Console.WriteLine("Falta cadastrada com sucesso: ");
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Aluno nao Encontrado!");
+                        }
+                        
+                        break;
+
+                    //consultar faltas
+                    case "6":
+                        Console.WriteLine("Entre com Nome ou Matricula do Aluno para Consulta nota:");
+                        aluno = disciplina.LocalizarAluno(Console.ReadLine());
+                        Console.WriteLine($"Aluno: {aluno.Nome} Matricula: {aluno.Matricula} Faltas: {disciplina.MapaDeFaltas[aluno]}");
+                        break;
+
+
+                    case "sair":
+                        Console.WriteLine("Saindo...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção invalida");
+                        break;
+                }
+                Console.ReadKey();
+
+            } while (entrada != "sair");
+        }
+    }
+}
